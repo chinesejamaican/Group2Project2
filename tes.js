0f517d8c74ad2866ae01f30c4edbcda4162a6f98
Student <student@cse.local>
2019-10-02 21:14:18 -0400

    adding script

--- tes.js
+++ tes.js
@@ -5,17 +5,18 @@ function runTime(){
 }
 
 function timee () {
-    tt = document.getElementById('test');
+    tt = document.getElementById('i1');
     let ttime = 0;
-    ttime = tt.innerHTML;
+    ttime = tt.value;
     let ta = [];
     ta = ttime.split(":");
     let time = new Date(2019,1,1,0,ta[0],ta[1],00)
-    time.setSeconds(time.getSeconds()- 1)
+    time.setSeconds(time.getSeconds() - 1)
     let min = time.getMinutes()
     let sec = time.getSeconds();
-    tt.innerHTML = min+":"+sec;
+    tt.value = min+":"+sec;
     
 }
 
-window.addEventListener("load", runTime);
+let s1 = document.getElementById("s1");
+s1.addEventListener("click",runTime);
